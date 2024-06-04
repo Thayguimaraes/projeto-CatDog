4a962b1925557080830c848fe5ea6207ee948b35
Leonardo Henrique Guimarães <leonardo.guimaraeslhg@gmail.com>
2024-06-04 16:19:38 -0300

    [Feat] Front

--- Front-End/src/app/clients-page/clients-page.component.ts
+++ Front-End/src/app/clients-page/clients-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { ClientService } from '../services/client.service';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-clients-page',
